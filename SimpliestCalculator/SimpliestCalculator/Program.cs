2772b3f823fb5b394e4cf4d7ac202186579f7e55
lessfe <vladislav_fedotov_00@mail.ru>
2023-04-19 15:28:41 +0300

    Commit for calculator

--- SimpliestCalculator/SimpliestCalculator/Program.cs
+++ SimpliestCalculator/SimpliestCalculator/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worldsadadasdas");
+﻿char again = 'д';
+while (again == 'д')
+{
+    double a;
+    double b;
+    double total;
+    char oper;
+
+    Console.WriteLine("Введите первое число:");
+    a = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите оператор:");
+    oper = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число:");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    if (oper == '+')
+    {
+        total = a + b;
+        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+    }
+
+    else if (oper == '-')
+    {
+        total = a - b;
+        Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+    }
+
+    else if (oper == '*')
+    {
+        total = a * b;
+        Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+    }
+
+    else if (oper == '/')
+    {
+        total = a / b;
+        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+    }
+    else
+    {
+        Console.WriteLine("Неизвестный оператор.");
+    }
+    Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+    again = Convert.ToChar(Console.ReadLine());
+}
+
+
+
+
